ed563cd31c83a77d490826aa63699977f2f3281f
Cblands <48147185+Cblands@users.noreply.github.com>
2020-08-25 16:11:47 -0700

    Update server.py

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import os
 
 application = Flask(__name__, static_folder='./build', static_url_path='/')
 
-@app.route('/')
+@application.route('/')
 def index():
     return application.send_static_file('index.html')
 
