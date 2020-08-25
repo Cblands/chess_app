ecbf30601535ea751df6258de61539e27c9afd1d
Cblands <48147185+Cblands@users.noreply.github.com>
2020-08-25 16:04:26 -0700

    m

--- server.py
+++ server.py
@@ -1,14 +1,14 @@
 from flask import Flask
 import os
 
-app = Flask(__name__, static_folder='./build', static_url_path='/')
+application = Flask(__name__, static_folder='./build', static_url_path='/')
 
 @app.route('/')
 def index():
-    return app.send_static_file('index.html')
+    return application.send_static_file('index.html')
 
 from engine.views import main
-app.register_blueprint(main)
+application.register_blueprint(main)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
+    application.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
