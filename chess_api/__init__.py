7dde8bae19756efe73c583590d452b2c566da8ae
Cblands <48147185+Cblands@users.noreply.github.com>
2020-08-25 14:20:40 -0700

    Update __init__.py

--- chess_api/__init__.py
+++ chess_api/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='../chess-engine-app/build', static_url_path='/')
+
+    @app.route('/')
+    def index():
+        return app.send_static_file('index.html')
 
     from .views import main
     app.register_blueprint(main)
